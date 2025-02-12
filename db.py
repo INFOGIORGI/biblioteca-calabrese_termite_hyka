43e1105aeb06f34d1d4ef08f1012aae62b6649ae
Zybb00 <zhyka19@gmail.com>
2025-02-12 11:32:44 +0000

    zaibev

--- db.py
+++ db.py
@@ -10,4 +10,43 @@ app.config['MYSQL_DB'] = 'w3schools'
 
 mysql = MySQL(app)
 
+def get_db_connection():
+    return mysql.connector.connect(
+        host=app.config['MYSQL_HOST'],
+        user=app.config['MYSQL_USER'],
+        password=app.config['MYSQL_PASSWORD'],
+        database=app.config['MYSQL_DB']
+    )
+
+@app.route('/create_table')
+def create_table():
+    conn = get_db_connection()
+    cursor = conn.cursor()
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS utenti (
+            id INT AUTO_INCREMENT PRIMARY KEY,
+            nome VARCHAR(100),
+            email VARCHAR(100)
+        )
+    ''')
+    conn.commit()
+    cursor.close()
+    conn.close()
+    return 
+
+@app.route("/")
+def index():
+
+    return render_template('index.html', titolo="Home page")
+
+@app.route("/profile")
+def profile():
+
+    return render_template('profile.html', titolo="Profile")
+
+@app.route("/users")
+def users():
+    
+    return render_template('users.html')
+
 app.run(debug=True)
