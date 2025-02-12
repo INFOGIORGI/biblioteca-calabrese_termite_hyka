4fe6304b8dd197df85584fe5372d6d436053d8e9
Zybb00 <zhyka19@gmail.com>
2025-02-12 11:46:50 +0000

    zyberr

--- db.py
+++ db.py
@@ -18,16 +18,63 @@ def get_db_connection():
         database=app.config['MYSQL_DB']
     )
 
+@app.route('/drop_table')
+def drop_table():
+    conn = get_db_connection()
+    cursor = conn.cursor()
+    cursor.execute('''
+        DROP TABLE IF EXISTS Libri
+DROP TABLE IF EXISTS Autori
+DROP TABLE IF EXISTS Utenti
+DROP TABLE IF EXISTS Prestiti
+DROP TABLE IF EXISTS Scrive
+    ''')
+    conn.commit()
+    cursor.close()
+    conn.close()
+    return
+
 @app.route('/create_table')
 def create_table():
     conn = get_db_connection()
     cursor = conn.cursor()
     cursor.execute('''
-        CREATE TABLE IF NOT EXISTS utenti (
-            id INT AUTO_INCREMENT PRIMARY KEY,
-            nome VARCHAR(100),
-            email VARCHAR(100)
-        )
+        CREATE TABLE Libri(
+    ISBNLibro char[13] PRIMARY KEY,
+    Categoria varchar [15] NOT NULL,
+    Titolo varchar [15] NOT NULL, 
+    Prezzo float NOT NULL,
+    Scaffale int NOT NULL,
+    Anno int NOT NULL,
+    CodAutore varchar [10] NOT NULL,
+
+     FOREIGN KEY CodAutore REFERENCES Autore(CodAutore),
+    )
+CREATE TABLE Autore(
+    CodAutore varchar [10] PRIMARY KEY,
+    Nome varchar [10] NOT NULL,
+    Cognome varchar [10] NOT NULL,
+    DataNascita date,
+    DataMorte date,
+    )
+
+CREATE TABLE Utente(
+    CodUtente varchar [10] PRIMARY KEY,
+    Nome varchar [10] NOT NULL,
+    Cognome varchar [10] NOT NULL,
+    Email varchar NOT NULL,
+    )
+
+CREATE TABLE PRESTITI(
+    CodPrestito varchar [10] PRIMARY KEY,
+    CodUtente varchar [10] PRIMARY KEY,
+    DataPrestito date,
+    DataRestituzione date,
+    ISBNLibro varchar[13],
+
+    FOREIGN KEY CodUtente REFERENCES Utente,
+    FOREIGN KEY ISBNLibro REFERENCES Libri
+    )
     ''')
     conn.commit()
     cursor.close()
