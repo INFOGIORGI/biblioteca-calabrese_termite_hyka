af3c9936ae17f8cb37aadf0ceefa4921725afa80
danielcalab <86962732+danielcalab@users.noreply.github.com>
2025-02-19 10:40:11 +0000

    commit1

--- app.py
+++ app.py
@@ -3,11 +3,17 @@ from db import db
 
 app = Flask(__name__)
 
-users = ['Alice', 'Bob', 'Charlie'] 
-
 @app.route("/")
 def hello():
     return render_template("index.html", message='Ciao mondo!!')
+
+@app.route("/users")
+def user():
+    return render_template('users.html', users=users)
+
+@app.route("/user/<utente>")
+def utente(utente):
+    return render_template('profile.html', utente=utente)
 
 
 app.run(debug=True)
--- db.py
+++ db.py
@@ -10,7 +10,6 @@ app.config['MYSQL_DB'] = 'calabrese-termite-hyka'
 
 mysql = MySQL(app)
 
-<<<<<<< HEAD
 def getUtenti():
     try:
         cursor = mysql.connection.cursor()
@@ -24,45 +23,3 @@ def getUtenti():
 
 if __name__ == '__main__':
     app.run(debug=True)
-=======
-def get_db_connection():
-    return mysql.connector.connect(
-        host=app.config['MYSQL_HOST'],
-        user=app.config['MYSQL_USER'],
-        password=app.config['MYSQL_PASSWORD'],
-        database=app.config['MYSQL_DB']
-    )
-
-@app.route('/create_table')
-def create_table():
-    conn = get_db_connection()
-    cursor = conn.cursor()
-    cursor.execute('''
-        CREATE TABLE IF NOT EXISTS utenti (
-            id INT AUTO_INCREMENT PRIMARY KEY,
-            nome VARCHAR(100),
-            email VARCHAR(100)
-        )
-    ''')
-    conn.commit()
-    cursor.close()
-    conn.close()
-    return 
-
-@app.route("/")
-def index():
-
-    return render_template('index.html', titolo="Home page")
-
-@app.route("/profile")
-def profile():
-
-    return render_template('profile.html', titolo="Profile")
-
-@app.route("/users")
-def users():
-    
-    return render_template('users.html')
-
-app.run(debug=True)
->>>>>>> 43e1105 (zaibev)
